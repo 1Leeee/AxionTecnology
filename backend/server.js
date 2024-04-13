aeeac05a5d3e4c1dcc01518c35c3d6d2b1b58868
1Leeee <brianleeortizramirez@gmail.com>
2024-04-12 23:21:11 -0400

    Armado de Proyecto

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// server.js
+import express from 'express';
+import cors from 'cors';
+import { logger } from 'logger-express';
+import dotenv from 'dotenv';
+import pool from './db/conectionDB.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors()); // Middleware de CORS
+app.use(logger());
+
+app.use('/api/v1', ); //colocar ruta de api
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
